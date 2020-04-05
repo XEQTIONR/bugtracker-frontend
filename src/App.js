86b33da4dde8638434332e0d56529d51fda54c1e
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2020-04-05 00:18:13 -0400

    Bootstrap and Jquery installation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid px-0">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
