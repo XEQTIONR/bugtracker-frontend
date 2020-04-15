793a509aeba0bd5e5b00505884ff8ffe0789e631
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2020-04-15 15:57:31 -0400

    Tasks Page Stub

--- src/components/pages/TasksPage.js
+++ src/components/pages/TasksPage.js
@@ -11,13 +11,12 @@ function TasksPage(props){
 
         <div className="row mx-2 mt-4">
           <div className="col-12">
-            <h1 className="ml-1">TasksPage</h1>
+            <h1 className="ml-1">Tasks Page</h1>
             <h5 className="ml-1 text-muted">Your Tasks</h5>
           </div>
         </div>
         <div className="row mx-2">
           <div className="col-md-3">
-            {/* <Button label="Create a new Issue" buttonClasses="btn btn-success btn-sm" /> */}
             <div className="row">
               <div className="col-12">
 
@@ -73,18 +72,37 @@ function TasksPage(props){
 
           </div>
           <div className="col-md-6">
-             <h3 className="pb-0">Please Create New 5GM 1st Upsell Split Test</h3> 
- 
-            <h6>Description</h6>
-            <p>We'd like to test functionality on the 5GM 1st upsell page here: https://5gmale.com/upsells/5gmale/upsell-6-month-supply.php that is currently present on the FS 1st upsell page. </p>
+             <div className="row pt-2">
+               <div className="col-10">
+                <h6><i className="fas fa-check-square"></i> MD-211</h6>
+               </div>
+               <div className="col-2">
+                 <button className="btn btn-sm p-0 d-block ml-auto mr-3" style={{position: "relative",top: "-4px"}}>
+                 <i className="fas fa-ellipsis-h"></i>
+                 </button>
+               </div>
+             </div>
+             <div className="row">
+               <div className="col-12">
+              <h3 className="pb-0">Please Create New 5GM 1st Upsell Split Test</h3> 
+              <h6>Description</h6>
 
-            <p>When the 5GM 1st upsell page is refreshed, it does not currently pop the buy buttons the way that the FS 1st upsell page does on refresh, and the way the 5GM/FS VSLs do in the front of the funnel. </p>
+               </div>
+             </div>
+            <div className="row">
+              <div className="col-12">
 
-            <p>Please add functionality to the page so that if the user refreshes or revisits the page the page loads with the buy buttons already present. </p>
+              <p>We'd like to test functionality on the 5GM 1st upsell page here: https://5gmale.com/upsells/5gmale/upsell-6-month-supply.php that is currently present on the FS 1st upsell page. </p>
 
-            <p>Please use master/control/v1 formatting for this test as well as LL variables.</p> 
+              <p>When the 5GM 1st upsell page is refreshed, it does not currently pop the buy buttons the way that the FS 1st upsell page does on refresh, and the way the 5GM/FS VSLs do in the front of the funnel. </p>
 
-            <p>I think that's it - we discussed this briefly on Skype so I'm not being super detailed in the ticket - let me know if I missed anything or if you have any questions. Thanks!</p>
+              <p>Please add functionality to the page so that if the user refreshes or revisits the page the page loads with the buy buttons already present. </p>
+
+              <p>Please use master/control/v1 formatting for this test as well as LL variables.</p> 
+
+              <p>I think that's it - we discussed this briefly on Skype so I'm not being super detailed in the ticket - let me know if I missed anything or if you have any questions. Thanks!</p>
+              </div>
+            </div> 
           </div>
 
           <div className="col-md-3">
@@ -110,37 +128,27 @@ function TasksPage(props){
 
               <div className="col-12 mt-4">
                 <h6 className="strong">Assigned to </h6>
-                <div class="user-block mt-1">
-                  <img class="img-circle img-bordered-sm" src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" alt="User Image" />
-                  <span class="username mt-1">
-                    <a href="#">Sarah Ross</a>
-                    {/* <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a> */}
-                  </span>
-                  {/* <span class="description">Sent you a message - 3 days ago</span> */}
+                <div className="user-block mt-1">
+                  <img className="img-circle img-bordered-sm" src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" alt="User Image" />
+                  <span className="username mt-1"><a href="#">Sarah Ross</a></span>
                 </div>
               </div>
 
               <div className="col-12 mt-4">
                 <h6 className="strong">Assigned by </h6>
-                <div class="user-block mt-1">
-                  <img class="img-circle img-bordered-sm" src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" alt="User Image" />
-                  <span class="username mt-1">
-                    <a href="#">Sarah Ross</a>
-                    {/* <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a> */}
-                  </span>
-                  {/* <span class="description">Sent you a message - 3 days ago</span> */}
+                <div className="user-block mt-1">
+                  <img className="img-circle img-bordered-sm" src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" alt="User Image" />
+                  <span className="username mt-1"><a href="#">Sarah Ross</a></span>
                 </div>
               </div>
 
               <div className="col-12 mt-4">
                 <h6 className="strong">Reported by </h6>
-                <div class="user-block mt-1">
-                  <img class="img-circle img-bordered-sm" src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" alt="User Image" />
-                  <span class="username mt-1">
+                <div className="user-block mt-1">
+                  <img className="img-circle img-bordered-sm" src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" alt="User Image" />
+                  <span className="username mt-1">
                     <a href="#">Sarah Ross</a>
-                    {/* <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a> */}
                   </span>
-                  {/* <span class="description">Sent you a message - 3 days ago</span> */}
                 </div>
               </div>
 
