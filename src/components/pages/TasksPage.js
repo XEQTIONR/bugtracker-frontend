e2742c7c801f66c543b3c3c34c6aaa04e9b823a9
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2020-04-15 01:00:04 -0400

    Cleaner markup for tasks page

--- src/components/pages/TasksPage.js
+++ src/components/pages/TasksPage.js
@@ -18,17 +18,55 @@ function TasksPage(props){
         <div className="row mx-2">
           <div className="col-md-3">
             {/* <Button label="Create a new Issue" buttonClasses="btn btn-success btn-sm" /> */}
+            <div className="row">
+              <div className="col-12">
 
-            <div className="input-group input-group-sm my-2">
-                    <input type="text" name="table_search" className="form-control float-right" placeholder="Search" />
+              <div className="input-group input-group-sm my-2">
+                      <input type="text" name="table_search" className="form-control float-right" placeholder="Search" />
 
-                    <div className="input-group-append">
-                      <button type="submit" className="btn btn-primary"><i className="fas fa-search"></i></button>
-                    </div>
-                    <div className="input-group-append">
-                      <button type="submit" className="btn btn-secondary"><i className="fa fa-filter"></i></button>
-                    </div>
-          </div>
+                      <div className="input-group-append">
+                        <button type="submit" className="btn btn-primary"><i className="fas fa-search"></i></button>
+                      </div>
+                      <div className="input-group-append">
+                        <button type="submit" className="btn btn-secondary"><i className="fa fa-filter"></i></button>
+                </div>
+              </div>
+              </div>
+
+            <div className="col-12">
+            <div className="card">
+
+
+            <div className="card-body list-group table-responsive p-0" style={{maxHeight: "75vh"}}>
+
+              <ul className="list-group list-group-flush">
+              <li className="list-group-item">Cras justo odio</li>
+              <li className="list-group-item">Dapibus ac facilisis in</li>
+              <li className="list-group-item">Morbi leo risus</li>
+              <li className="list-group-item">Porta ac consectetur ac</li>
+              <li className="list-group-item">Vestibulum at eros</li>
+              <li className="list-group-item">Cras justo odio</li>
+              <li className="list-group-item">Dapibus ac facilisis in</li>
+              <li className="list-group-item">Morbi leo risus</li>
+              <li className="list-group-item">Porta ac consectetur ac</li>
+              <li className="list-group-item">Vestibulum at eros</li>
+              <li className="list-group-item">Cras justo odio</li>
+              <li className="list-group-item">Dapibus ac facilisis in</li>
+              <li className="list-group-item">Morbi leo risus</li>
+              <li className="list-group-item">Porta ac consectetur ac</li>
+              <li className="list-group-item">Vestibulum at eros</li>
+              <li className="list-group-item">Cras justo odio</li>
+              <li className="list-group-item">Dapibus ac facilisis in</li>
+              <li className="list-group-item">Morbi leo risus</li>
+              <li className="list-group-item">Porta ac consectetur ac</li>
+              <li className="list-group-item">Vestibulum at eros</li>
+              </ul>
+            </div>
+            </div>
+
+            </div>
+
+            </div>
 
 
 
@@ -36,6 +74,17 @@ function TasksPage(props){
           </div>
           <div className="col-md-6">
              <h3 className="pb-0">Please Create New 5GM 1st Upsell Split Test</h3> 
+ 
+            <h6>Description</h6>
+            <p>We'd like to test functionality on the 5GM 1st upsell page here: https://5gmale.com/upsells/5gmale/upsell-6-month-supply.php that is currently present on the FS 1st upsell page. </p>
+
+            <p>When the 5GM 1st upsell page is refreshed, it does not currently pop the buy buttons the way that the FS 1st upsell page does on refresh, and the way the 5GM/FS VSLs do in the front of the funnel. </p>
+
+            <p>Please add functionality to the page so that if the user refreshes or revisits the page the page loads with the buy buttons already present. </p>
+
+            <p>Please use master/control/v1 formatting for this test as well as LL variables.</p> 
+
+            <p>I think that's it - we discussed this briefly on Skype so I'm not being super detailed in the ticket - let me know if I missed anything or if you have any questions. Thanks!</p>
           </div>
 
           <div className="col-md-3">
@@ -57,68 +106,6 @@ function TasksPage(props){
               </div>
               </div>
             </div>
-          </div>
-
-
-        </div>
-
-        <div className="row mx-2">
-          <div className="col-md-3">
-
-          <div className="card">
-
-
-              <div className="card-body list-group table-responsive p-0" style={{maxHeight: "75vh"}}>
-
-                <ul className="list-group list-group-flush">
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
-                <li className="list-group-item">Cras justo odio</li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
-                </ul>
-              </div>
-            
-            </div>
-
-          </div>
-          <div className="col-md-6">
-            {/* <div className="card"> */}
-              {/* <div className="card-body pt-2"> */}
-             
-
-              <h6>Description</h6>
-
-
-
-              <p>We'd like to test functionality on the 5GM 1st upsell page here: https://5gmale.com/upsells/5gmale/upsell-6-month-supply.php that is currently present on the FS 1st upsell page. </p>
-
-              <p>When the 5GM 1st upsell page is refreshed, it does not currently pop the buy buttons the way that the FS 1st upsell page does on refresh, and the way the 5GM/FS VSLs do in the front of the funnel. </p>
-
-              <p>Please add functionality to the page so that if the user refreshes or revisits the page the page loads with the buy buttons already present. </p>
-
-              <p>Please use master/control/v1 formatting for this test as well as LL variables.</p> 
-
-              <p>I think that's it - we discussed this briefly on Skype so I'm not being super detailed in the ticket - let me know if I missed anything or if you have any questions. Thanks!</p>
-              {/* </div> */}
-            {/* </div> */}
-          </div>
-          <div className="col-md-3">
             <div className="row">
 
               <div className="col-12 mt-4">
@@ -159,7 +146,11 @@ function TasksPage(props){
 
             </div>
           </div>
+
+
         </div>
+
+        
     </Page>
   )
 
